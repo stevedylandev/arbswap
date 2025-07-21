a95c936f6750bea771ad24c371ad807e63662035
Steve Simkins <stevedsimkins@gmail.com>
2025-07-20 22:50:46 -0400

    chore: Updated styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { TopClankers } from "./components/TopClankers";
 import type { Token } from "./services/tokenService";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "./components/ui/button";
+import arbLogo from "./assets/arb-logo.svg";
 
 function App() {
 	const { isConnected } = useAccount();
@@ -34,11 +35,34 @@ function App() {
 	};
 
 	return (
-		<div className="max-w-4xl w-full mx-auto flex flex-col gap-6 items-center justify-center min-h-screen p-4">
-			<h1 className="text-3xl font-bold">ArbSwap</h1>
-			<p className="text-center text-muted-foreground mb-4">
-				Swap tokens on Arbitrum using Farcaster
-			</p>
+		<div className="max-w-4xl w-full mx-auto flex flex-col gap-6 items-center justify-start min-h-screen p-4 mt-4">
+			<div className="flex flex-col gap-2 items-center justify-center">
+				<img src={arbLogo} alt="arbitrum logo" className="w-24" />
+				<h1 className="text-3xl font-bold text-white">ArbSwap</h1>
+				<p className="text-center text-[#E5E5E5] mb-4">
+					Swap tokens on Arbitrum using Farcaster
+				</p>
+				<p className="text-center text-white text-xs flex items-center">
+					Powered by
+				</p>
+				<p className="text-center text-white text-lg flex items-center">
+					{" "}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						className="h-6 w-6"
+					>
+						<title>divide</title>
+						<path
+							fill="currentColor"
+							d="M6 11h12a1 1 0 0 1 0 2H6a1 1 0 0 1 0-2m6.002-7a2 2 0 1 0-.004 4a2 2 0 0 0 .004-4m0 12a2 2 0 1 0-.004 4a2 2 0 0 0 .004-4"
+						/>
+					</svg>{" "}
+					Quotient
+				</p>
+			</div>
 			{isConnected && (
 				<Tabs defaultValue="search" className="w-full">
 					<TabsList className="w-full">
