18e1e1d13e24594dda723061e9afbe8f8c1a1e1e
Steve Simkins <stevedsimkins@gmail.com>
2025-07-20 20:50:27 -0400

    fix: Fixed build error

--- client/src/components/TopClankers.tsx
+++ client/src/components/TopClankers.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { Users, ArrowRight } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { sdk } from "@farcaster/miniapp-sdk";
 import { useState } from "react";
 
